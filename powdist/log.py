6c1e9f90303d6fb7e69802eed15de385d7d257d4
pglotfel <pglotfel@gmail.com>
2018-10-26 13:24:27 -0400

    more files

---
+++ powdist/log.py
@@ -0,0 +1,33 @@
+import logging
+import threading
+
+_lock = threading.Lock()
+_logger = None
+_name = 'root'
+_level = logging.DEBUG
+
+
+def get_logger():
+    """Returns a "global" logger.
+
+    Args:
+        level (optional): The logging level.
+        name (optional): The name of the logger.
+
+    """
+
+    global _lock
+    global _logger
+
+    with _lock:
+        if(_logger is None):
+            formatter = logging.Formatter(fmt='%(asctime)s:%(levelname)s:%(module)s - %(message)s')
+            handler = logging.StreamHandler()
+            handler.setFormatter(fmt=formatter)
+            _logger = logging.getLogger(_name)
+            _logger.addHandler(handler)
+            _logger.setLevel(_level)
+            # Don't propogate up to root logger
+            _logger.propagate = False
+
+        return _logger
